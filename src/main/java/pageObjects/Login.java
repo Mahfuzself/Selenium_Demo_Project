c2d6fcc040118ec4759936040201f7732cc481e9
Masbah Tanzil <tanzil16@gmail.com>
2025-02-13 12:16:38 +0600

    Config Rizz pharama

--- src/main/java/pageObjects/Login.java
+++ src/main/java/pageObjects/Login.java
@@ -1,12 +1,17 @@
 package pageObjects;
+import java.util.List;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import testBase.TestBase;
 
 public class Login  extends TestBase{
-	
-	
+	@FindBy (id="dnn_ctr_Login_Login_DNN_txtUsername")
+	WebElement inputEmailAddress;
+	@FindBy (id="dnn_ctr_Login_Login_DNN_txtPassword")
+	WebElement inputPasswordt;
 	
 	
  public	Login (){
